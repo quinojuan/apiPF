d0d4debcdc0c2e603ea2faffd18299a6ec7f7004
Juan Quino <quinojuan@gmail.com>
2022-09-15 12:24:32 -0300

    db corregida

--- src/db.js
+++ src/db.js
@@ -4,10 +4,22 @@ const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/movilgates`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/movilgates`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL || DATABASE_LOCAL, {
+	dialectOptions: {
+	  ssl: {
+	    require: true,
+	    rejectUnauthorized: false,
+	  },
+	},
+	logging: false, // set to console.log to see the raw SQL queries
+	native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
