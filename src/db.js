d36dc5a6548aeb295a94b3b123f4b78ad3536a4b
Juan Quino <quinojuan@gmail.com>
2023-02-12 23:01:20 -0300

    ordenando la db

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || 
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/movilgates`,
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 	{
 	  dialectOptions: {
 		ssl: {
